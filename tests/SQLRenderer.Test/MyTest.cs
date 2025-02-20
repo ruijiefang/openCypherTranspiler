6b805491f5f40fa89c86593985c1bfc994750d34
Ruijie Fang <ruijief@princeton.edu>
2025-02-20 16:39:10 -0600

    update

--- tests/SQLRenderer.Test/MyTest.cs
+++ tests/SQLRenderer.Test/MyTest.cs
@@ -18,15 +18,20 @@ using openCypherTranspiler.CommonTest;
 using openCypherTranspiler.openCypherParser;
 using openCypherTranspiler.LogicalPlanner;
 using openCypherTranspiler.SQLRenderer.Test;
+using Newtonsoft.Json;
+using openCypherTranspiler.Common.GraphSchema;
+
 
 namespace openCypherTranspiler.SQLRenderer.MyTest
 {
+
+    
     [TestClass]
     public class MyTester
     {    
         private readonly ILoggable _logger = new TestLogger(LoggingLevel.Normal);
 
-        private ISQLDBSchemaProvider _graphDef;
+        private JSONGraphSQLSchema _graphDef;
 
 
         [TestInitialize]
@@ -35,16 +40,80 @@ namespace openCypherTranspiler.SQLRenderer.MyTest
             _graphDef = new JSONGraphSQLSchema(@"./TestData/MovieGraph.json");
         }
 
-        private string TranspileToSQL(string cypherQueryText)
+        private string TranspileToSQL(string cypherQueryText, JSONGraphSQLSchema graphDef)  
         {
-            var parser = new OpenCypherParser(_logger);
-            var queryNode = parser.Parse(cypherQueryText);
-            var plan = LogicalPlan.ProcessQueryTree(
-                parser.Parse(cypherQueryText),
-                _graphDef,
-                _logger);
-            var sqlRender = new SQLRenderer(_graphDef, _logger);
-            return sqlRender.RenderPlan(plan);
+            try
+            {
+                var parser = new OpenCypherParser(_logger);
+                var queryNode = parser.Parse(cypherQueryText);
+                var plan = LogicalPlan.ProcessQueryTree(
+                    parser.Parse(cypherQueryText),
+                    graphDef,
+                    _logger);
+                var sqlRender = new SQLRenderer(_graphDef, _logger);
+                return sqlRender.RenderPlan(plan);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("error: " + e.ToString());
+            }
+        }
+        
+        
+        public static List<(string CypherQuery, string FileName)> ReadJsonLines(string filePath)
+        {
+            var results = new List<(string, string)>();
+
+            // Read the file line by line
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+                var r = new JsonTextReader(new StringReader(line));
+                var serializer = new JsonSerializer();
+                try
+                {
+                    string[] values = serializer.Deserialize<string[]>(r);
+                    if (values != null && values.Length == 2)
+                    {
+                        results.Add((values[0], values[1]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid line format (expected exactly 2 elements): " + line);
+                    }
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine("Error parsing line: " + line);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return results;
+        }
+
+        [TestMethod]
+        public void ReadGraphitiAndOutput()
+        {
+            var prefix = "./TestData/";
+            var fileName = prefix+"graphiti-test.jsonlines";
+
+            var l = ReadJsonLines(fileName);
+            foreach (var i in l)
+            {
+                /*Console.WriteLine($"[{i.CypherQuery}, {i.FileName}");*/
+                var schema = new JSONGraphSQLSchema(i.FileName);
+                try
+                {
+                    var result = TranspileToSQL(i.CypherQuery, schema);
+                    Console.WriteLine($"[\"{i.CypherQuery}\", \"{i.FileName}\", \"{result.ToString().Replace('\n', ' ')}\"]");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[{i.CypherQuery}, {i.FileName}, \"ERROR: ${e.ToString().Substring(0, Math.Min(30,e.ToString().Length-1))}\"]");
+                }
+            }
         }
 
         [TestMethod]
@@ -58,11 +127,13 @@ LIMIT 20
 WHERE Title <> 'A'
 RETURN  Title, Name
     ";*/
-         var queryText = @"
+    /*     var queryText = @"
 MATCH (p:Person)-[a:ACTED_IN]->(m:Movie)
 RETURN  m.Title, p.Name
-";
-            var result = TranspileToSQL(queryText);
+";*/
+    var queryText = @"
+OPTIONAL MATCH (p:Person)-[a:ACTED_IN]->(m) RETURN p.Name";
+            var result = TranspileToSQL(queryText, _graphDef);
             Console.WriteLine($"transpiled result: {result}");
         }
         
